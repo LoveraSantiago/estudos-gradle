5edaf769c02e98ed2fcf79a89f0f9926bce898fb
Santiago Lovera <santiago.lovera@gmail.com>
2017-03-11 09:56:25 -0300

    11032017

--- 2.10-Demo-JavaUnitTests/src/test/java/com/udacity/gradle/test/PersonTest.java
+++ 2.10-Demo-JavaUnitTests/src/test/java/com/udacity/gradle/test/PersonTest.java
@@ -9,4 +9,16 @@ public class PersonTest {
         Person person = new Person("Jeremy");
         assert person.getName().equals("Jeremy");
     }
+
+     @Test
+    public void test2() {
+        Person person = new Person("Santuga");
+        assert person.getName().equals("Jeremy");
+    }
+
+     @Test
+    public void test3() {
+        Person person = new Person("");
+        assert person.getName().equals("");
+    }
 }
